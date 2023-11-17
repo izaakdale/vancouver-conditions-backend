9a80d1d0a1e3256e17e4d71b7d44ddf5ef75898d
Izaak Dale <izaakdaledev@gmail.com>
2023-11-17 00:43:16 -0800

    integrated with new api

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/izaakdale/vancouver-conditions-backend
 
 go 1.21.1
 
-require github.com/gorilla/websocket v1.5.1
-
-require golang.org/x/net v0.17.0 // indirect
+require github.com/rs/cors v1.10.1
--- main.go
+++ main.go
@@ -2,33 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	// allow origins
-	upgrader := websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
-		return true
-		// org := r.Header.Get("Origin")
-		// return org == "http://localhost:5173"
-	}}
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Print("upgrade failed: ", err)
-			return
-		}
-		defer conn.Close()
-
-		f, err := os.Open("./data.json")
+	mux.HandleFunc("/stub", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("./composite-data.json")
 		if err != nil {
 			panic(err)
 		}
@@ -39,47 +23,13 @@ func main() {
 			panic(err)
 		}
 
-		toSend := ResortData{
-			ID:               data.ID,
-			Name:             data.Name,
-			Country:          data.Country,
-			Continent:        data.Continent,
-			CurrentBaseTempC: data.Forecast[0].Base.TempC,
-		}
-
-		if data.Forecast[0].RainMm == 0 && data.Forecast[0].SnowMm == 0 {
-			toSend.PrecipitationStatus = "Still"
-		}
-		if data.Forecast[0].RainMm > data.Forecast[0].SnowMm {
-			toSend.PrecipitationStatus = "Raining"
-		} else {
-			toSend.PrecipitationStatus = "Snowing"
-		}
-
-		var snow1, snow3, snow5 float64
-		for i, f := range data.Forecast {
-			if i < 8 {
-				snow1 += f.Base.FreshsnowCm
-			} else if i < 30 {
-				snow3 += f.Base.FreshsnowCm
-			} else {
-				snow5 += f.Base.FreshsnowCm
-			}
-		}
-		toSend.SnowFall1Day = snow1
-		toSend.SnowFall3Days = snow1 + snow3
-		toSend.SnowFall5Days = snow1 + snow3 + snow5
-
-		var i int
-		for {
-			if err := conn.WriteJSON(toSend); err != nil {
-				// probably due to client close, break to trigger deferred close of conn
-				break
-			}
-			time.Sleep(time.Second * 10)
-			i++
+		w.Header().Add("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(data)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
 		}
 	})
 
-	http.ListenAndServe("localhost:8080", mux)
+	http.ListenAndServe("localhost:8080", cors.Default().Handler(mux))
 }
